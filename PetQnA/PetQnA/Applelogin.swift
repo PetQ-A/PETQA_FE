2b9d4bf55a462d1b08bf8755cbfa8426535baac2
gayound <thstndus020@gmail.com>
2024-08-01 16:16:39 +0900

    Update Applelogin.swift

--- PetQnA/PetQnA/Applelogin.swift
+++ PetQnA/PetQnA/Applelogin.swift
@@ -5,40 +5,81 @@
 ////  Created by 김가영 on 7/15/24.
 ////
 //
-//@objc func clickAppleLogin(_ sender: UITapGestureRecognizer) {
-//   
-//    let request = ASAuthorizationAppleIDProvider().createRequest()
-//    request.requestedScopes = [.fullName, .email]
-//
-//    let controller = ASAuthorizationController(authorizationRequests: [request])
-//    controller.delegate = self as? ASAuthorizationControllerDelegate
-//    controller.presentationContextProvider = self as? ASAuthorizationControllerPresentationContextProviding
-//    controller.performRequests()
-//
-//    extension LoginVC: ASAuthorizationControllerDelegate {
-//        // 성공
-//        func authorizationController(controller: ASAuthorizationController, didComplete authorization: ASAuthorization) {
-//            if let credential = authorization.credential as? ASAuthorizationAppleIDCredential {
-//
-//                let idToken = credential.identityToken!
-//                let tokeStr = String(data: idToken, encoding: .utf8)
-//                print(tokeStr)
-//
-//                guard let code = credential.authorizationCode else { return }
-//                let codeStr = String(data: code, encoding: .utf8)
-//                print(codeStr)
-//
-//                let user = credential.user
-//                print(user)
-//
-//            }
-//        }
-//
-//        // 실패
-//        func authorizationController(controller: ASAuthorizationController, didError error: Error) {
-//            print("error")
-//        }
-//    }
-//}
-// 
-//
+
+import AuthenticationServices
+
+extension SignInAppleVC: ASAuthorizationControllerPresentationContextProviding, ASAuthorizationControllerDelegate{
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
+    // Apple ID 연동 성공 시
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential {
+            // Apple ID
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            
+            // 계정 정보 가져오기
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            let idToken = appleIDCredential.identityToken!
+            let tokeStr = String(data: idToken, encoding: .utf8)
+         
+            print("User ID : \(userIdentifier)")
+            print("User Email : \(email ?? "")")
+            print("User Name : \((fullName?.givenName ?? "") + (fullName?.familyName ?? ""))")
+            print("token : \(String(describing: tokeStr))")
+            
+        default:
+            break
+        }
+    }
+    
+    // Apple ID 연동 실패 시
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // Handle error.
+    }
+}
+
+class SignInAppleVC: UIViewController {
+//애플 소셜 로그인
+    @IBAction func appleLogin(_ sender: Any) {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+}
+
+//applelogin 상태 확인
+func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        //forUserID = userIdentifier
+        appleIDProvider.getCredentialState(forUserID: "001281.9301aaa1f617423c9c7a64b671b6eb84.0758") { (credentialState, error) in
+            switch credentialState {
+            case .authorized:
+                // The Apple ID credential is valid.
+                print("해당 ID는 연동되어있습니다.")
+            case .revoked:
+                // The Apple ID credential is either revoked or was not found, so show the sign-in UI.
+                print("해당 ID는 연동되어있지않습니다.")
+            case .notFound:
+                // The Apple ID credential is either was not found, so show the sign-in UI.
+                print("해당 ID를 찾을 수 없습니다.")
+            default:
+                break
+            }
+        }
+        //앱 실행 중 강제로 연결 취소 시
+        NotificationCenter.default.addObserver(forName: ASAuthorizationAppleIDProvider.credentialRevokedNotification, object: nil, queue: nil) { (Notification) in
+            print("Revoked Notification")
+            // 로그인 페이지로 이동
+        }
+    return true
+}
